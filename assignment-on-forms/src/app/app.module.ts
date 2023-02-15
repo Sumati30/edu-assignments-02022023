a97ded9b73b316dc39c9d118674b2e18f921e797
Sumati30 <sumati.khed30@gmail.com>
2023-02-15 17:56:01 +0530

    Second commit

--- assignment-on-forms/src/app/app.module.ts
+++ assignment-on-forms/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveFormsComponent } from './reactive-forms/reactive-forms.component';
+import { TemplateDrivenFormsComponent } from './template-driven-forms/template-driven-forms.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReactiveFormsComponent,
+    TemplateDrivenFormsComponent
   ],
   imports: [
     BrowserModule,
