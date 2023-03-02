b4792d8f95ae5202ce0c800b4ace5113923334d5
Sumati30 <sumati.khed30@gmail.com>
2023-03-02 22:49:12 +0530

    Login-Auth-Logout added

--- assignment-on-forms/src/app/app-routing.module.ts
+++ assignment-on-forms/src/app/app-routing.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
 import { ReactiveFormsComponent } from './reactive-forms/reactive-forms.component';
 import { TemplateDrivenFormsComponent } from './template-driven-forms/template-driven-forms.component';
+import { AuthGuard } from './helpers/auth.guard'
 
 
 const routes: Routes = [
+  {path:'', component:HomeComponent , canActivate:[AuthGuard]},
   {path:'reactive-form', component:ReactiveFormsComponent},
-  {path:'template-form', component:TemplateDrivenFormsComponent}
+  {path:'template-form', component:TemplateDrivenFormsComponent},
+  {path:'login', component:LoginComponent},
+  {path:'register', component:RegisterComponent},
+  {path:'**' , redirectTo:''}
 ];
 
 @NgModule({
--- assignment-on-forms/src/app/app.module.ts
+++ assignment-on-forms/src/app/app.module.ts
@@ -1,28 +1,50 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { FormsModule } from '@angular/forms';
+import { ComparePasswordsDirective } from './directives/compare-passwords.directive';
+
 import { ReactiveFormsComponent } from './reactive-forms/reactive-forms.component';
 import { TemplateDrivenFormsComponent } from './template-driven-forms/template-driven-forms.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { FormsModule } from '@angular/forms';
-import { ComparePasswordsDirective } from './directives/compare-passwords.directive'
+
+
+import { HttpClientModule,HTTP_INTERCEPTORS } from '@angular/common/http';
+import { fakeBackendProvider } from './helpers/fake-backend';
+import { AlertComponent } from './alert/alert.component'
+
+import { JwtInterceptor } from './helpers/jwt.interceptor';
+import { ErrorInterceptor } from './helpers/error_interceptor'
 
 @NgModule({
   declarations: [
     AppComponent,
-    ReactiveFormsComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    AlertComponent,
     TemplateDrivenFormsComponent,
+    ReactiveFormsComponent,
     ComparePasswordsDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     ReactiveFormsModule,
+    AppRoutingModule,
+    HttpClientModule,
     FormsModule
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+    fakeBackendProvider],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
